53eb9052c08b2ae51f48ead42480becf4397fd08
Sam Francisco <samueljacob.francisco@alorica.com>
2017-12-15 00:07:02 +0800

    Styles for fields with error

--- electron/profile.js
+++ electron/profile.js
@@ -12,12 +12,6 @@ const endpoints = settings.read(settingsKeys.helpMeEndpoints);
 
 $(document).ready(function() {
 
-  $('.icon-search').click(function() {
-    let profileField = $(this).attr('id');
-    settings.save(settingsKeys.lastProfileFieldSearch, profileField);
-    ipcRenderer.send('search-profile-field');
-  });
-
   ipcRenderer.on('load-profile-field', (event, arg) => {
     console.log("event: ", event);
     console.log("arg: ", arg);
@@ -30,6 +24,59 @@ $(document).ready(function() {
     else if (profileField == "clients") {
       $('#client').val(name);
     }
+  });
+
+  $('.icon-search').click(function() {
+    let profileField = $(this).attr('id');
+    settings.save(settingsKeys.lastProfileFieldSearch, profileField);
+    ipcRenderer.send('search-profile-field');
+  });
+
+  $('#save_profile').click(function(e) {
+    let location = _.trim($('#location').val());
+    let businessUnit = _.trim($('#business_unit').val());
+    let businessUnitName = $('#business_unit option:selected').text();
+    let client = _.trim($('#client').val());
+    let callBackNumber = _.trim($('#call_back_number').val());
+    let hasError = false;
+
+    if (!location) {
+      hasError = true;
+      $('#location').addClass('has-error');
+    }
+    else {
+      $('#location').removeClass('has-error');
+    }
+
+    if (!businessUnit) {
+      hasError = true;
+      $('#business_unit').addClass('has-error');
+    }
+    else {
+      $('#business_unit').removeClass('has-error');
+    }
+
+    if (!client) {
+      hasError = true;
+      $('#client').addClass('has-error');
+    }
+    else {
+      $('#client').removeClass('has-error');
+    }
+
+    if (!callBackNumber) {
+      hasError = true;
+      $('#call_back_number').addClass('has-error');
+    }
+    else {
+      $('#call_back_number').removeClass('has-error');
+    }
+
+    if (hasError) {
+      return;
+    }
+
+
   });
 
 });
